9e488a8dce83452bec362d97d8ad038abd185168
leonleidner <leidnerleon@gmail.com>
2025-11-22 12:52:32 +0100

    Revert basePath to debug styling

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? '/text-to-sql-agent' : '',
+  // basePath: isProd ? '/text-to-sql-agent' : '',
   images: {
     unoptimized: true,
   },
